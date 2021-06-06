70d02415ef90ece7a602b8fb76ba2fee7078dbb2
Евгений Кулеша <tms-21-online/>
2021-06-06 22:17:56 +0300

    task_3_2_commit
    choice of location

--- Hw_3.py
+++ Hw_3.py
@@ -1,4 +1,11 @@
 number = 5000
 if number % 1000 == 0:
     print("millennium")
-    
+
+number_of_guests = 33
+if number_of_guests > 50:
+    print('restaurant')
+elif number_of_guests >= 20:
+    print('cafe')
+else:
+    print('home')
