305fe88f69e9f2dc4010a27bc83336b74cb5bad0
Евгений Кулеша <tms-21-online/>
2021-06-07 11:54:11 +0300

    task_3_09_commit - не разобрался, Гугл мне в помощь

--- Hw_3.py
+++ Hw_3.py
@@ -66,6 +66,27 @@ number_one = (input('input your happy number '))
 if number_one.isdigit():
     print(int(number_one) ** 3)
 
+import math
+
+print("Введите коэффициенты для уравнения")
+print("ax^2 + bx + c = 0:")
+a = float(input("a = "))
+b = float(input("b = "))
+c = float(input("c = "))
+
+discr = b ** 2 - 4 * a * c
+print("Дискриминант D = %.2f" % discr)
+
+if discr > 0:
+    x1 = (-b + math.sqrt(discr)) / (2 * a)
+    x2 = (-b - math.sqrt(discr)) / (2 * a)
+    print("x1 = %.2f \nx2 = %.2f" % (x1, x2))
+elif discr == 0:
+    x = -b / (2 * a)
+    print("x = %.2f" % x)
+else:
+    print("Корней нет")
+
 
 
 
